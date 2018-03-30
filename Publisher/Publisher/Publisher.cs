cd43cf23d9c93988d25b7d00697e25b80c32987f
Daniel Wallace <b-rad10@comcast.net>
2018-03-30 13:46:51 -0400

    Create C# Publisher Project

---
+++ Publisher/Publisher/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Publisher
+{
+    class Publisher
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
